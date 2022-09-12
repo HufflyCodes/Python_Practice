e721e3c77fd8f044f57c9989d0349c2efa855506
HufflyCodes <Wesley.c.huff@gmail.com>
2022-09-12 13:33:20 -0400

    Added prompts for clarity

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -1,3 +1,15 @@
+### PROMPT ###
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+#An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically
+#using all the original letters exactly once.
+
+### CONSTRAINTS ###
+# 1 <= strs.length <= 104
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
+
+
 import itertools
 strs = ["eat","tea","tan","ate","nat","bat"] #input
 sortstrs = strs.copy()
--- Image_Rotate.py
+++ Image_Rotate.py
@@ -1,8 +1,24 @@
+### PROMPT ###
+
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+# Example 1:
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+### CONSTRAINTS ###
+# n == matrix.length == matrix[i].length
+# 1 <= n <= 20
+# -1000 <= matrix[i][j] <= 1000
+
 import numpy as np
 
 pic = np.array([[1,2,3],[4,5,6],[7,8,9]])
 pic2 = ([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
 print(pic)
-print(np.rot90(pic,k=3))
+print(np.rot90(pic,k=-1))
 
 # Not that efficient more efficient would be transposing then flipping about the y axis (reverse rows)
+# Most efficient would be modifying it in C either using transposes and flipping or rotation matrices
